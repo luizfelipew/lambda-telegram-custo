82bfa0cfbddcb66dd3db371093981bafc847bdd8
luizfelipew <luizfelipewendt@gmail.com>
2024-08-05 20:12:49 -0300

    Lendo dados do CE

--- projeto3/handler.py
+++ projeto3/handler.py
@@ -1,10 +1,28 @@
 import json
+import boto3
 
+sessao = boto3.Session(profile_name='automacao-curso')
+cliente_ce = sessao.client('ce')
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v3.0! Your function executed successfully!",
-        "input": event,
+def get_cost(event, context):
+    
+    resposta = cliente_ce.get_cost_and_usage(
+            TimePeriod={
+                'Start': '2024-08-01',
+                'End': '2024-09-01',
+            },
+            Granularity='MONTHLY',
+            Metrics=[
+                'AmortizedCost'
+            ]
+        )
+    valor = resposta['ResultsByTime'][0]['Total']['AmortizedCost']['Amount']
+    valor = round(float(valor), 2)
+
+    mensagem = f"O custo da AWS é USD ${valor}."
+
+    return {
+        "statusCode": 200
     }
 
-    return {"statusCode": 200, "body": json.dumps(body)}
+get_cost({}, {})
